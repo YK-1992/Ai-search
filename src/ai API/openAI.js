535fefca595c9c94f4ae7f67acb8eb1bcef38dfd
YK-1992 <179270256+YK-1992@users.noreply.github.com>
2025-05-04 20:55:27 +0200

    light

--- src/ai API/openAI.js
+++ src/ai API/openAI.js
@@ -2,7 +2,7 @@ import { OpenAI } from "openai";
 
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1", 
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY, 
+  apiKey: import.meta.env.VITE_OPENROUTER_API_KEY, 
   dangerouslyAllowBrowser: true,
 });
 export const fetchStudyExplanation = async (question, level) => {
